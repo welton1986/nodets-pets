b2fd9d2282a1f77cd4dacc4fafb75c4d56567894
Welton Cardoso <bryan_july@hotmail.com>
2022-03-22 15:44:47 -0300

    Final Project 1.0

--- src/controllers/seachController.ts
+++ src/controllers/seachController.ts
@@ -1,5 +1,19 @@
 import {Request, Response} from 'express';
 
-export const search = (req : Request, res : Response) =>{
+import {Pet} from '../models/pet';
 
+export const search = (req : Request, res : Response) =>{
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page',{
+       list,
+       query
+    })
 }
--- src/server.ts
+++ src/server.ts
@@ -20,8 +20,8 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-    res.send('Página não encontrada.')
-})
+    res.render('pages/404');
+});
 
 
 
